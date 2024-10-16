cf90672bbe99797c375234b0a0870acc3aad77d7
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-10-16 21:08:12 +0300

    add: task 2 (ex01) started

---
+++ day_02/src/exercise1/main.py
@@ -0,0 +1,32 @@
+"""
+Downloading pictures asyncronical
+"""
+
+import os
+import asyncio
+
+
+class PictureSaver:
+    def __init__(self):
+        self.save_directory = None
+        self.statuses = {"test": True}
+
+    def check_save_directory(self) -> bool:
+        try:
+            inputed_path = input()
+            abs_path = os.path.abspath(inputed_path)
+            if not os.access(abs_path, os.W_OK):
+                raise Exception
+            self.save_directory = abs_path
+        except Exception:
+            print("Пожалуйста, выбери другой путь для сохранения файлов:")
+            self.check_save_directory()
+
+
+async def main():
+    pic_saver = PictureSaver()
+    pic_saver.check_save_directory()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
